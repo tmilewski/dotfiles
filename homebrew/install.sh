1f655cb433c9a56a71c02fff2188d24d05e8ef8d
Tom Milewski <me@tm.codes>
2025-01-04 23:36:12 -0500

    fix: eval homebrew on install

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -10,6 +10,7 @@ if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 exit 0
