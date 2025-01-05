290369fbf50fd96273cf575015df233684556add
Tom Milewski <me@tm.codes>
2025-01-04 22:27:22 -0500

    chore: Add Bun to PATH

--- node/path.zsh
+++ node/path.zsh
@@ -1,7 +1,13 @@
 export N_PREFIX=$HOME/.n
 export PATH=$N_PREFIX/bin:$PATH
 
-export PNPM_HOME="~/Library/pnpm"
+export BUN_BIN=$HOME/.bun/bin
+case ":$PATH:" in
+  *":$BUN_BIN:"*) ;;
+  *) export PATH="$BUN_BIN:$PATH" ;;
+esac
+
+export PNPM_HOME=$HOME/Library/pnpm
 case ":$PATH:" in
   *":$PNPM_HOME:"*) ;;
   *) export PATH="$PNPM_HOME:$PATH" ;;
